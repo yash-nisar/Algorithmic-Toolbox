3427ec6e91b5421463fe73f5076581accf91788e
yash-nisar <yash.nisar@somaiya.edu>
2016-08-29 16:16:26 +0530

    Add A_plus_B.cpp

---
+++ Week 1/A_plus_B.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+
+Given two digits a and b, find a + b.
+
+*/
+
+int main(){
+
+    int a, b;
+    
+    cin >> a >> b;
+    cout << a + b;
+
+    return 0;
+}
